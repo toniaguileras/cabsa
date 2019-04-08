a2b50ff5ea91475828612c46e39ba54ebb9e0876
Toni <antonio.aguilera@concatel.com>
2019-04-08 08:59:15 +0200

    developing clean test

--- src/test/java/es/cabsa/javadevelopers/test/JungleControllerTest.java
+++ src/test/java/es/cabsa/javadevelopers/test/JungleControllerTest.java
@@ -1,24 +1,40 @@
 package es.cabsa.javadevelopers.test;
 
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import javax.transaction.Transactional;
+
+import org.jboss.jandex.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import es.cabsa.javadevelopers.utils.URLConstants;
+
+
 @Transactional
-// @RunWith(SpringJUnit4ClassRunner.class)
-// @ContextConfiguration(classes = { ApplicationConfig.class })
-// @WebAppConfiguration
+ @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = { Main.class})
+ @WebAppConfiguration
 public class JungleControllerTest
 {
-//	private MockMvc mockMvc;
+	private MockMvc mockMvc;
 
-//	@Test
-//	public void testFindAnimals()
-//	{
-//		mockMvc.perform(get(URLConstants.JUNGLE_ROOT + URLConstants.ANIMAL_ROOT).contentType(MediaType.APPLICATION_JSON)
-//				.andExpect(status().isOk()).andExpect(jsonPath("$[0].id").value(0))
-//				.andExpect(jsonPath("$[0].name").value("Rabbit")).andExpect(jsonPath("$[0].legs").value(4)));
-//	}
+	@Test
+	public void testFindAnimals()
+	{
+		mockMvc.perform(get(URLConstants.JUNGLE_ROOT + URLConstants.ANIMAL_ROOT).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(jsonPath("$[0].id").value(0))
+				.andExpect(jsonPath("$[0].name").value("Rabbit")).andExpect(jsonPath("$[0].legs").value(4));
+	}
 //
 //	@Test
 //	public void testFindAnimalsWithItsFood()
